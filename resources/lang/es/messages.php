3be465ab5dc67d2bc7b6be953081fc83f1ec05b0
s1uggard <alexeygeno@gmail.com>
2023-08-02 13:57:19 +0300

    Add the es lang

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'otp' => 'Tu código es :code',
+    'initiation_rate_limit' => 'Puedes enviar solo :sms sms en :hours horas',
+    'completion_rate_limit' => 'Está intentando utilizar un código incorrecto :times veces en :minutes minutos',
+    'initiation_success' => 'Se ha enviado un sms. ¡Revisa tu teléfono!',
+    'completion_success' => '¡La verificación está hecha!',
+    'expired' => 'El código está caducado. Solo tienes :minutes minutos para usarlo',
+    'incorrect' => 'El código es incorrecto'
+];
